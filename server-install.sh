4ec8b5f4eccb4308b6a03bb9be0da4e473665334
Your Name <you@example.com>
2021-01-22 21:34:19 +0000

    removed a bunch of questions

--- server-install.sh
+++ server-install.sh
@@ -16,50 +16,31 @@ echo "
 \"I added some ASCII art because you can't have a bash script without ASCII art\"  - @plenumlab								   
 "
 
-echo "Would you like to install CouchDB first? (Requires dcoker.io & docker-compose.)"
-PS3="Please select an option (1 Or 2): "
-choices=("yes" "no")
-select choice in "${choices[@]}"; do
-        case $choice in
-                yes)
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
-                                        echo "Downloading CouchDb docker-compose file"
-                                        curl -sSL https://raw.githubusercontent.com/bitnami/bitnami-docker-couchdb/master/docker-compose.yml > docker-compose.yml
-										echo "Replacing default password and removing ports 9100,4369"
-										adminpass=$(openssl rand -base64 32)
+curl -sSL https://raw.githubusercontent.com/bitnami/bitnami-docker-couchdb/master/docker-compose.yml > docker-compose.yml
+echo "Replacing default password and removing ports 9100,4369"
+adminpass=$(openssl rand -base64 32)
 										
-										# Check if script is ran on MacOs, if so: use extra single quotes in SED command.
-										if [[ "$OSTYPE" == "darwin"* ]]; then MACOS="''"; fi
-										
-										sed -i $MACOS "s|=couchdb|=$adminpass|g" docker-compose.yml
-										sed -i $MACOS "s|- '4369:4369'||g" docker-compose.yml
-										sed -i $MACOS "s|- '9100:9100'||g" docker-compose.yml
-										echo "Your administrator username is admin"
-										echo "Your administrator password is $adminpass"
-                                        docker-compose up -d
-                                        sleep 1
-                                        break
-                                        ;;
-                no)
-                                        echo "Skipping CouchDB installation ..."
-                                        sleep 1
-                                        break
-                                        ;;
-        esac
-done
+if [[ "$OSTYPE" == "darwin"* ]]; then MACOS="''"; fi
+sed -i $MACOS "s|=couchdb|=$adminpass|g" docker-compose.yml
+sed -i $MACOS "s|- '4369:4369'||g" docker-compose.yml
+sed -i $MACOS "s|- '9100:9100'||g" docker-compose.yml
+echo "Your administrator username is admin"
+echo "Your administrator password is $adminpass"
+docker-compose up -d
+sleep 1
 
-echo -e "To configure the BBRF CouchDB server, please enter your: "
-read -p 'CouchDB administrator: ' admin
-read -p 'Administrator password: ' passwd
-read -p 'CouchDB URL in format [proto]://[host]:[port]: ' url
+
+admin="admin"
+passwd="$adminpass"
+url="http://$(curl -s ifconfig.me):5984"
 
 echo "Creating BBRF database..."  
 curl -u $admin:$passwd -X PUT $url/bbrf 
-
-echo "Please enter the details for a new user in CouchDB."
-read -p 'Username: ' user
-read -p 'Password (leave blank to generate a strong one): ' upass
-[ -z "$upass" ] && upass=$(openssl rand -base64 32) && echo "Password for low-privilege user is: $upass"
+user="pry"
+upass=$(openssl rand -base64 32) && echo "Password for low-privilege user is: $upass"
 
 echo "Creating the low-privilege user..."
 curl -u $admin:$passwd -X PUT $url/_users/org.couchdb.user:$user \
@@ -73,13 +54,7 @@ curl -u $admin:$passwd -X PUT $url/bbrf/_security -d "{\"admins\": {\"names\": [
 echo "Creating required views..."
 curl -u $user:$upass -X PUT $url/bbrf/_design/bbrf -d @views.json
 
-
-
 echo -e "\nCreate a (default) config file under ~/.bbrf/config.json :\n"
 echo -e "{\n\"username\": \"$user\",\n \"password\": \"$upass\",\n\"couchdb\": \"$url/bbrf\",\n \"slack_token\": \"$slack\"\n}" 
-
-
-echo -e "\nBelow is an example of the bash function :\n"
-echo -e "function bbrf() {\nsource $PWD/.env/bin/activate; \npython $PWD/bbrf.py \"\$@\" \ndeactivate \n}\n"
 
 echo "Server setup completed..."
